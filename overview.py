17c60df10983e8921095c5879e3c48192cdbb2ec
ahn <math.ansang@gmail.com>
2019-02-01 16:32:12 +0900

    overview start

--- overview.py
+++ overview.py
@@ -1,21 +1,23 @@
-import  xenarix as xen
+import xenarix as xen
+import xenarix.results as xen_r
+
 import model_generate as mg
 
 scenSetID='scenSet1'
 scenID='scenID'
 resultID='resultID'
 
-mg.generate(scenSet=scenSetID,
-            scen=scenID,
-            result=resultID)
+mg.generate(scenSetID=scenSetID,
+            scenID=scenID,
+            resultID=resultID)
 
-result = xen.ResultObj()
+result = xen_r.ResultObj(scenSetID, scenID, resultID)
 
-result_multipath = result.get_multipath(scen_count=1)
+multipath = result.get_multipath(scen_count=1)
 
 calculated_result = []
 
-for t, v in result.timegrid, result_multipath:
+for t, v in result.timegrid, multipath:
 
     v[0]
     calculated_result.append([])
